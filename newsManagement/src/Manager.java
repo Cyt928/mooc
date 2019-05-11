de24fa2d2a36693e29aa13d2593a8e0b7523ffed
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2019-05-11 15:04:36 +0800

    0

--- newsManagement/src/Manager.java
+++ newsManagement/src/Manager.java
@@ -10,12 +10,18 @@ public class Manager extends Worker {
 	}
 	//Manager类的初始化
 	public Manager(String name, int age, int salary, String department) {
-
+        if (age < 18 || salary < 2000) {
+            String message = "age must be greater than 18 and salary must be greater than 2000.";
+            throw new IllegalArgumentException(message);
+        }
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.department = department;
 	}
 
 	// 管理人员可以查询本部门员工的基本信息，跨部门查询提示权限不足，提示“Access Denied!”
 	public String inquire(Worker e) {
-		//判断是否为本部门员工
 		if(e.getDepartment().equals(this.department)){
 		    return e.show();
         }else{
@@ -25,12 +31,21 @@ public class Manager extends Worker {
 
 	// 管理人员给自己的队伍添加工作人员，同一部门的工作人员可以添加，并返回true，不同部门的工作人员无法添加，返回false
 	public boolean lead(Worker e) {
-		return false;
+	    if(e.getDepartment().equals(this.department)){
+	        worker.add(e);
+	        return true;
+        }else {
+            return false;
+        }
 	}
 
 	// 打印自己队伍的人员姓名，没有打印“Empty”
 	public String print() {
-		return null;
+		String str="Statement for "+this.name+"\n";
+		for(Worker temp : worker ){
+		    str=str+" - "+temp.getName()+"\n";
+        }
+        return str;
 	}
 
 }
