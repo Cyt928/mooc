bf5ba99944192a249535973ae1d5d247afdb2f23
Cyt928 <36220406+Cyt928@users.noreply.github.com>
2019-05-11 14:39:27 +0800

    0

--- newsManagement/src/Manager.java
+++ newsManagement/src/Manager.java
@@ -15,7 +15,12 @@ public class Manager extends Worker {
 
 	// 管理人员可以查询本部门员工的基本信息，跨部门查询提示权限不足，提示“Access Denied!”
 	public String inquire(Worker e) {
-		return null;
+		//判断是否为本部门员工
+		if(e.getDepartment().equals(this.department)){
+		    return e.show();
+        }else{
+		    throw new IllegalArgumentException("Access Denied!");
+        }
 	}
 
 	// 管理人员给自己的队伍添加工作人员，同一部门的工作人员可以添加，并返回true，不同部门的工作人员无法添加，返回false
